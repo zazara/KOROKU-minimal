7f1856315a20e8f2bd4829a1103b66798c206283
zazara <porinum1@gmail.com>
2019-08-22 00:51:17 +0900

    Add Copyright notification

--- src/Canvas.h
+++ src/Canvas.h
@@ -1,3 +1,22 @@
+/* KOROKU-minimal
+ *
+ * Copyright (C) 2019 zazara
+ *
+ * This file is part of KOROKU-minimal.
+ *
+ * KOROKU-minimal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KOROKU-minimal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KOROKU-minimal.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #define _USE_MATH_DEFINES
 #include <bits/stdc++.h>
 #include <cairomm/context.h>
